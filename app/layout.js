8709a69d4fda34a767e6b47bfd364fde9d8ed785
MD TANVIR ISLAM <mail.tanvirdev@gmail.com>
2026-03-09 19:13:09 +0600

    Project

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Layout from "@/apps/layout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,12 +19,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <Layout>
+      <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
     </html>
+
+    </Layout>
+
+
   );
 }
---
+++ apps/layout/Header.jsx
@@ -0,0 +1,14 @@
+import { headers } from 'next/headers'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='bg-white'>
+      <div className="container mx-auto">
+        hello
+      </div>
+    </header>
+  )
+}
+
+export default Header
